73fb952040920ecbb7e1b0e89faec2d761075fb5
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-28 04:23:42 +0300

    Решение_задача_54

---
+++ Tema_3_HW_8/Tema_3_HW_7_Primer_54/Program.cs
@@ -0,0 +1,106 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+
+// Функция задает количество строк в массиве
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+        }
+    }
+
+    return result;
+}
+
+
+// Функция инициализирует массив
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(-99, 100);
+        }
+    }
+
+    return matrix;
+}
+
+
+// Функция выводит массив на экран
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+// Функция сортировки элементов в строке двумерного массива, по убыванию
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+int countOfRows = GetNumber("Введите кол-во строк:");
+
+int countOfColumns = GetNumber("Введите кол-во столбцов:");
+
+int[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+Console.WriteLine();
+
+PrintMatrix(matrix);
+
+Console.WriteLine();
+
+SortToLower(matrix);
+
+PrintMatrix(matrix);
